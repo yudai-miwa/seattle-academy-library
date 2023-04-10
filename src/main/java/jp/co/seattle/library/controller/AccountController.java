0606a87e19869bf38d3b888710ad6293998aecdd
miwa-yudai <miwa-yudai@localhost>
2023-04-10 17:57:57 +0900

    task1１次コミット

--- src/main/java/jp/co/seattle/library/controller/AccountController.java
+++ src/main/java/jp/co/seattle/library/controller/AccountController.java
@@ -48,14 +48,24 @@ public class AccountController {
 		logger.info("Welcome createAccount! The client locale is {}.", locale);
 
 		// バリデーションチェック、パスワード一致チェック（タスク１）
-
-		
-		// パラメータで受け取ったアカウント情報をDtoに格納する。
-		UserInfo userInfo = new UserInfo();
-		userInfo.setEmail(email);
-		userInfo.setPassword(password);
-		usersService.registUser(userInfo);
-		return "redirect:/login";
+		if (password.length() >= 8 && password.matches("^[A-Za-z0-9]+$")) {
+			if (password.equals(passwordForCheck)) {
+				
+				// パラメータで受け取ったアカウント情報をDtoに格納する。
+				UserInfo userInfo = new UserInfo();
+				userInfo.setEmail(email);
+				userInfo.setPassword(password);
+				usersService.registUser(userInfo);
+				return "redirect:/login";
+				
+			} else {
+				model.addAttribute("errorMessage", "確認用パスワードと一致しません。");
+				return "createAccount";
+			}
+		} else {
+			model.addAttribute("errorMessage", "半角英数字８文字で以上で入力してください。");
+			return "createAccount";
+		}
 	}
 
 }
