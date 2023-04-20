ff5a3aea7937007974ffca67a77951bbb4e135dc
miwa-yudai <miwa-yudai@localhost>
2023-04-20 11:52:28 +0900

    Task5　2次コミット

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -94,7 +94,7 @@ public class BooksService {
 					bookInfo.getPublishDate(), bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
 		} else {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "UPDATE books SET title = ?, author = ?, publisher = ?, publish_date = ?,ThumbnailName = ?,ThumbnailUrl = ?, isbn = ?, description = ?, upd_date = now() WHERE id = ?";
+			sql = "UPDATE books SET title = ?, author = ?, publisher = ?, publish_date = ?,thumbnailname = ?,thumbnailurl = ?, isbn = ?, description = ?, upd_date = now() WHERE id = ?";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getThumbnailName(), bookInfo.getThumbnailUrl(),
 					bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
