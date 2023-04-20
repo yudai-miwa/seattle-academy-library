1a629ec57d2d61272ebaedd55051fba650c95013
miwa-yudai <miwa-yudai@localhost>
2023-04-20 14:19:44 +0900

    Task5 3次コミット

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -61,7 +61,7 @@ public class BooksService {
 	 */
 	public int registBook(BookDetailsInfo bookInfo) {
 		// TODO 取得した書籍情報を登録し、その書籍IDを返却するようにSQLを修正（タスク４）
-		String sql = "";
+		String sql = "INSERT into books(title, author, publisher, publish_date,thumbnail_url, thumbnail_name, isbn, description, reg_date,upd_date)VALUES(?,?,?,?,?,?,?,?,now(),now()) RETURNING id;";
 
 		int bookId = jdbcTemplate.queryForObject(sql, int.class, bookInfo.getTitle(), bookInfo.getAuthor(),
 				bookInfo.getPublisher(), bookInfo.getPublishDate(), bookInfo.getThumbnailName(),
@@ -89,12 +89,12 @@ public class BooksService {
 		String sql;
 		if (bookInfo.getThumbnailUrl() == null) {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "UPDATE books SET title = ?,authr = ?,publisher = ?,publish_date = ?,isbn = ?,description = ?,upd_date = now()WHERE id = ? ";
+			sql = "UPDATE books SET title = ?,author = ?,publisher = ?,publish_date = ?,isbn = ?,description = ?,upd_date = now()WHERE id = ?";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
 		} else {
 			// TODO 取得した書籍情報を更新するようにSQLを修正（タスク５）
-			sql = "UPDATE books SET title = ?, author = ?, publisher = ?, publish_date = ?,thumbnailname = ?,thumbnailurl = ?, isbn = ?, description = ?, upd_date = now() WHERE id = ?";
+			sql = "title = ?, author = ?, publisher = ?, publish_date = ?,thumbnail_name = ?,thumbnail_url = ?, isbn = ?, description = ?, upd_date = now() WHERE id = ?";
 			jdbcTemplate.update(sql, bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
 					bookInfo.getPublishDate(), bookInfo.getThumbnailName(), bookInfo.getThumbnailUrl(),
 					bookInfo.getIsbn(), bookInfo.getDescription(), bookInfo.getBookId());
